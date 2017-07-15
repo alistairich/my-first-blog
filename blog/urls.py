d181fade1f69128bd98defd298844c16f0dfee57
alistairich <arccat24@outlook.com>
2017-07-15 16:03:55 +0800

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url 
 from . import views
 
+
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
@@ -12,4 +13,5 @@ urlpatterns = [
 	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 	url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
 	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+	url(r'^signup/$', views.signup, name='signup'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post, Comment
@@ -84,3 +86,17 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
