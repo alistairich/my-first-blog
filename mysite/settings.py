91f3f5b56f2ee9bd342c49828a01acaae5dafa6a
alistairich <arccat24@outlook.com>
2017-08-09 10:25:06 +0800

    Arranged templates and add buttons and change color.

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
 	'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       	'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       	#'ENGINE': 'django.db.backends.mysql',
        	#'ENGINE': 'mysql.connector.django',
-       	'NAME': 'blog',
-       	'USER': 'jasmine',
-       	'PASSWORD': 'alistair21',
-       	'HOST': 'localhost',
-       	'PORT': '',
+       	#'NAME': 'blog',
+       	#'USER': 'jasmine',
+       	#'PASSWORD': 'alistair21',
+       	#'HOST': 'localhost',
+       	#'PORT': '',
     }
 }
 
